a3ff6260f63a416bd4bf73133753b3f34bf6b27a
ianllow <128955358+ianllow@users.noreply.github.com>
2024-10-08 21:57:57 +1100

    Update vegalitevis.js

--- vegalitevis.js
+++ vegalitevis.js
@@ -1,10 +1,10 @@
 var vg_1 = "https://raw.githubusercontent.com/ianllow/A2/main/visualization.vl.json";
-var vg_2 = "https://raw.githubusercontent.com/ianllow/A2/main/barchart.vg";
+var vg_2 = "https://raw.githubusercontent.com/ianllow/A2/main/linechart.vg";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("##barchart", vg_2).then(function(result) {
+vegaEmbed("#linechart", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);w
