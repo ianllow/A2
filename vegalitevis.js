792dfd7e7c07bcc05f3697bbf2983df2408fba23
ianllow <128955358+ianllow@users.noreply.github.com>
2024-10-13 15:06:40 +1100

    Add files via upload

--- vegalitevis.js
+++ vegalitevis.js
@@ -1,15 +1,16 @@
 var vg_1 = "https://raw.githubusercontent.com/ianllow/A2/main/visualization.vl.json";
 var vg_2 = "https://raw.githubusercontent.com/ianllow/A2/main/linechart.vg";
-var vg_3 = "https://raw.githubusercontent.com/ianllow/A2/main/bubbleplot.vg";
+var vg_3 = "https://raw.githubusercontent.com/ianllow/A2/main/donutchart.vg";
+var vg_4 = "https://raw.githubusercontent.com/ianllow/A2/main/bubbleplot.vg";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed("#linechart", vg_2).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#bubbleplot", vg_3).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#donutchart", vg_3).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#bubbleplot", vg_4).then(function(result) {
 }).catch(console.error);
